9d09cd255a55355f921b42afa6662596d2c90d03
Jatin Katyal <jatin.katyal13@gmail.com>
2019-07-21 20:43:55 +0530

    settings update

---
+++ MyStaticFile/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class MystaticfileConfig(AppConfig):
+    name = 'MyStaticFile'
--- PortfolioGenerator/settings.py
+++ PortfolioGenerator/settings.py
@@ -30,6 +30,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -113,5 +114,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 PORTFOLIO_DATA = os.path.join(BASE_DIR, 'data.json')
